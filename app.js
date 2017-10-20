9bc7ee6b030310f6dc1aaca4b386360980753ac1
Ivaylo <bloodydoom3@gmail.com>
2017-10-20 22:43:54 +0300

    setup three.js and webstorm

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let renderer = new THREE.WebGLRenderer({
     canvas: document.getElementById('myCanvas'), antialias: true
-})
+});
 
 renderer.setClearColor(0x011330);
 renderer.setPixelRatio(window.devicePixelRatio);
@@ -10,19 +10,18 @@ let camera = new THREE.PerspectiveCamera(35, window.innerWidth/window.innerHeigh
 camera.position.z=10;
 let scene = new THREE.Scene();
 
-let plane = new THREE.PlaneGeometry(2,2,2)
+let plane = new THREE.PlaneGeometry(2,2,2);
 
 let material = new THREE.MeshBasicMaterial();
 
 let cube = new THREE.BoxGeometry(2,2,2);
-let planeMesh = new THREE.Mesh(plane, material)
+let planeMesh = new THREE.Mesh(plane, material);
 let cubeMesh = new THREE.Mesh(cube, material);
 
 cubeMesh.position.set(0,0,-10);
-planeMesh.position.set(0,0,-40)
+planeMesh.position.set(0,0,-40);
 
 //scene.add(cubeMesh);
 scene.add(planeMesh);
 renderer.render(scene, camera);
 
-render
