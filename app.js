e173963e019f1d813730e2a5ddab6bdf5829dbfd
Ivaylo <bloodydoom3@gmail.com>
2017-10-25 01:54:57 +0300

    setting up light and orbit control

--- app.js
+++ app.js
@@ -1,27 +1,53 @@
 let renderer = new THREE.WebGLRenderer({
     canvas: document.getElementById('myCanvas'), antialias: true
 });
-
-renderer.setClearColor(0x011330);
+renderer.setClearColor('#010912');
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 
-let camera = new THREE.PerspectiveCamera(35, window.innerWidth/window.innerHeight, 1, 3000);
-camera.position.z=10;
 let scene = new THREE.Scene();
 
-let plane = new THREE.PlaneGeometry(2,2,2);
+let sphere = new THREE.IcosahedronGeometry(0.3,2);
+let material = new THREE.MeshPhongMaterial({
+    color: '#ff3920',
+    specular: '#ff8848',
+    shading: THREE.FlatShading,
+    transparent: 1,
+    opacity: 1});
 
-let material = new THREE.MeshBasicMaterial();
+let camera = new THREE.PerspectiveCamera(45, window.innerWidth/window.innerHeight, 1, 3000);
+camera.position.set(0,0,0);
+camera.lookAt(new THREE.Vector3(0, 0, 1));
 
-let cube = new THREE.BoxGeometry(2,2,2);
-let planeMesh = new THREE.Mesh(plane, material);
-let cubeMesh = new THREE.Mesh(cube, material);
 
-cubeMesh.position.set(0,0,-10);
-planeMesh.position.set(0,0,-40);
+let sphereMesh = new THREE.Mesh(sphere, material);
+sphereMesh.position.set(0,0,3);
 
-//scene.add(cubeMesh);
-scene.add(planeMesh);
-renderer.render(scene, camera);
+let light = new THREE.DirectionalLight(0xffffff,1);
+light.target.position.set(0,0,-5);
 
+let light2 = new THREE.DirectionalLight(0xffffff,4);
+light2.position.set(0,0,0)
+light2.target.position.set(0,0,-5)
+
+
+//scene.add(light);
+scene.add(light2);
+scene.add(sphereMesh);
+
+function render(){
+    //slight2.position.z-=0.1;
+    renderer.render(scene,camera);
+    requestAnimationFrame(render)
+}
+for(var i=0; i<500; i++){
+    let star = new THREE.IcosahedronGeometry(0.3,2);
+}
+
+let controls = new THREE.OrbitControls(camera);
+
+controls.object.position.set(0,0,0);
+controls.target = new THREE.Vector3(0, 0, 1);
+controls.update();
+
+render()
